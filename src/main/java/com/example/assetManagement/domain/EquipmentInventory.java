8ba40e489f3296d3ed927117374e038fe289243f
unknown <yenugubala.hari@gmail.com>
2019-11-01 15:53:39 +0530

    efewetwet

--- src/main/java/com/example/assetManagement/domain/EquipmentInventory.java
+++ src/main/java/com/example/assetManagement/domain/EquipmentInventory.java
@@ -3,17 +3,22 @@ package com.example.assetManagement.domain;
 import java.util.Date;
 
 import javax.persistence.CascadeType;
+import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 import org.hibernate.validator.constraints.br.TituloEleitoral;
 
+
+@Entity
+@Table(name = "equipmentinventory")
 public class EquipmentInventory {
 	
 	@Id
--- src/main/java/com/example/assetManagement/domain/SparePartInventry.java
+++ src/main/java/com/example/assetManagement/domain/SparePartInventry.java
@@ -3,16 +3,20 @@ package com.example.assetManagement.domain;
 import java.util.Date;
 
 import javax.persistence.CascadeType;
+import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
+@Entity
+@Table(name = "Sparepartinventry")
 public class SparePartInventry {
 	
 	@Id
